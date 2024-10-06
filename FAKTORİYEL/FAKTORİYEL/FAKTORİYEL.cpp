1bd358ac36fbbd28a11de73e4aca69e46e61fc8a
İsmail Demir <109111495+demiris20@users.noreply.github.com>
2024-10-06 15:08:15 +0300

    Add files via upload

---
+++ FAKTORİYEL/FAKTORİYEL/FAKTORİYEL.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()                                           
+{
+	int sayi, x;
+	int y = 1;
+	cout << "sayi giriniz :";
+	cin >> sayi;
+
+	for (x = 1; x <=sayi; x++) {
+		y *= x;
+	}
+	cout << "faktoriyeli =\t" << y;
+	
+	return 0;
+}
+
